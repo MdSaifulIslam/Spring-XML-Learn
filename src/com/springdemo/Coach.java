50fa51f57dbb9362d624c709df3d88f979a433b8
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2022-09-06 14:46:39 +0600

    Comments and day 01 Final

--- src/com/springdemo/Coach.java
+++ src/com/springdemo/Coach.java
@@ -1,6 +1,7 @@
 package com.springdemo;
 
 public interface Coach {
+	
 	public String getDailyWorkout();
 
 }
--- src/com/springdemo/SpringHelloApp.java
+++ src/com/springdemo/SpringHelloApp.java
@@ -7,7 +7,13 @@ public class SpringHelloApp {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = 
 				new ClassPathXmlApplicationContext("applicationContext.xml");
-		
+		/*
+		 * Pass the parent class to get Bean 
+		 * Here id = "myCoach"
+		 * class = "com.springdemo.BaseballCoach"  
+		 * implements the Coach.java named parent class
+		 * 
+		 * */
 		Coach theCoach = context.getBean("myCoach", Coach.class);
 		
 		System.out.println(theCoach.getDailyWorkout());
